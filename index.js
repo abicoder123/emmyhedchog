60eed19d88fd661fe3116092a98b74013da0c693
abicoder123 <ablor32@icstudents.org>
2026-02-28 18:04:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+const hedchog = document.getElementById("hedchog");
+document.addEventListner("keydown", function (a) {
+  if(a.key === "ArrowRight") player.style.left = player.offsetLeft + 10 + "px";
+  if(a.key === "ArrowLeft") player.style.left = player.offsetLeft + 10 + "px";
+  if(a.key === "ArrowUp") player.style.top = player.offsetTop + 10 + "px";
+  if(a.key === "ArrowDown") player.style.top = player.offsetTop + 10 + "px";
+
+
+    }
