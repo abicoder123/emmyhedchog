9668987d1efced4a0de10cd6ca588534794e5f29
abicoder123 <ablor32@icstudents.org>
2026-02-28 19:31:25 -0600

    dad cahnges

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const hedchog = document.getElementById("hedchog");
+const mylabel = document.getElementById("debug");
 
 document.addEventListener("keydown", function (a) {
   if (a.key === "ArrowRight") {
     hedchog.style.left = hedchog.offsetLeft + 10 + "px";
+    mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
   }
 
   if (a.key === "ArrowLeft") {
     hedchog.style.left = hedchog.offsetLeft - 10 + "px";
+    mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
   }
 
   if (a.key === "ArrowUp") {
     hedchog.style.top = hedchog.offsetTop + 10 + "px";
+    mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
+
   }
 
   if (a.key === "ArrowDown") {
     hedchog.style.top = hedchog.offsetTop - 10 + "px";
+    mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
     // hedchog.style.top = hedchog.offsetBottom + 10 + "px";
   }
 });
