63a1fa2c7d3300b3904a7d17be1fe3afd0aa5f88
abicoder123 <ablor32@icstudents.org>
2026-03-01 08:14:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+// const hedchog = document.getElementById("hedchog");
+// const mylabel = document.getElementById("debug");
+
+// let hedgeY=50;
+
+// document.addEventListener("keydown", function (a) {
+//   if (a.key === "ArrowRight") {
+//     hedchog.style.left = hedchog.offsetLeft + 10 + "px";
+//     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
+//   }
+
+//   if (a.key === "ArrowLeft") {
+//     hedchog.style.left = hedchog.offsetLeft - 10 + "px";
+//     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
+//   }
+
+//   if (a.key === "ArrowUp") {
+//     hedgeY = hedgeY-10;
+//     hedchog.style.top = hedgeY + "px";
+    
+//     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
+//     // hedchog.style.top = hedchog.offsetBottom + 10 + "px";
+
+//   if (a.key === "ArrowDown") {
+//         hedgeY = hedgeY+10;
+//     hedchog.style.top = hedgeY + "px";
+//     //hedchog.style.top = hedchog.offsetTop - 10 + "px";
+    
+//     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
+
+//   }
+// });
+
 const hedchog = document.getElementById("hedchog");
 const mylabel = document.getElementById("debug");
-
-let hedgeY=50;
+let hedgeY = hedchog.offsetTop;
 
 document.addEventListener("keydown", function (a) {
+
   if (a.key === "ArrowRight") {
     hedchog.style.left = hedchog.offsetLeft + 10 + "px";
     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
@@ -15,18 +48,15 @@ document.addEventListener("keydown", function (a) {
   }
 
   if (a.key === "ArrowUp") {
-    hedgeY = hedgeY-10;
+    hedgeY -= 10;
     hedchog.style.top = hedgeY + "px";
-    
     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
-    // hedchog.style.top = hedchog.offsetBottom + 10 + "px";
+  }
 
   if (a.key === "ArrowDown") {
-        hedgeY = hedgeY+10;
+    hedgeY += 10;
     hedchog.style.top = hedgeY + "px";
-    //hedchog.style.top = hedchog.offsetTop - 10 + "px";
-    
     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
-
   }
+
 });
