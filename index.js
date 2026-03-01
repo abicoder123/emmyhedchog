1b3f986708ff57a56a0502c431cd831d53d6798a
abicoder123 <ablor32@icstudents.org>
2026-02-28 18:35:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-
 const hedchog = document.getElementById("hedchog");
-document.addEventListner("keydown", function (a) {
-  if(a.key === "ArrowRight") player.style.left = player.offsetLeft + 10 + "px";
-  if(a.key === "ArrowLeft") player.style.left = player.offsetLeft + 10 + "px";
-  if(a.key === "ArrowUp") player.style.top = player.offsetTop + 10 + "px";
-  if(a.key === "ArrowDown") player.style.top = player.offsetTop + 10 + "px";
 
+document.addEventListener("keydown", function (a) {
+  if (a.key === "ArrowRight") {
+    hedchog.style.left = hedchog.offsetLeft + 10 + "px";
+  }
 
-    }
+  if (a.key === "ArrowLeft") {
+    hedchog.style.left = hedchog.offsetLeft - 10 + "px";
+  }
+
+  if (a.key === "ArrowUp") {
+    hedchog.style.top = hedchog.offsetTop - 10 + "px";
+  }
+
+  if (a.key === "ArrowDown") {
+    hedchog.style.top = hedchog.offsetTop + 10 + "px";
+  }
+});
