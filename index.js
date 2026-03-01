7101058b0a0b831bc50825a7e337af666a75cf96
abicoder123 <ablor32@icstudents.org>
2026-02-28 19:38:51 -0600

    Update index.js by dad

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hedchog = document.getElementById("hedchog");
 const mylabel = document.getElementById("debug");
 
+let hedgeY=50;
+
 document.addEventListener("keydown", function (a) {
   if (a.key === "ArrowRight") {
     hedchog.style.left = hedchog.offsetLeft + 10 + "px";
@@ -13,13 +15,18 @@ document.addEventListener("keydown", function (a) {
   }
 
   if (a.key === "ArrowUp") {
-    hedchog.style.top = hedchog.offsetTop - 10 + "px";
+    hedgeY = hedgeY+10;
+    hedchog.style.top = hedgeY + "px";
+    //hedchog.style.top = hedchog.offsetTop - 10 + "px";
+    
     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
 
   }
 
   if (a.key === "ArrowDown") {
-    hedchog.style.top = hedchog.offsetTop - 10 + "px";
+    hedgeY = hedgeY-10;
+    hedchog.style.top = hedgeY + "px";
+    
     mylabel.textContent = `${hedchog.style.left}, ${hedchog.style.top}`;
     // hedchog.style.top = hedchog.offsetBottom + 10 + "px";
   }
